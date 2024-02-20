a47ee00c9f4b5ca8d119afe2e0556c4b76d02daf
RICHITHA REDDY <104646601+RichithaaReddy@users.noreply.github.com>
2024-02-20 15:03:38 +0530

    languages

--- app.py
+++ app.py
@@ -2,6 +2,45 @@ import PIL
 import streamlit as st
 from ultralytics import YOLO
 import requests
+import cv2
+import numpy as np
+import tempfile
+import os
+import io
+# class_label_to_disease = {
+#     0: "Alternaria",
+#     1: "Anthracnose",
+#     2: "Bacterial Blight",
+#     3: "Cercospora",
+#     5: "Healthy",
+# }
+class_info = {
+    0: {
+        'name': 'Alternaria',
+        'description': 'This class represents a unhealthy pomegranate.',
+        'translation_key': 'disease0_translation_key',
+    },
+    1: {
+        'name': 'Anthracnose',
+        'description': 'This class represents the first type of pomegranate disease.',
+        'translation_key': 'disease1_translation_key',
+    },
+    2: {
+        'name': 'Bacterial_Blight',
+        'description': 'This class represents the second type of pomegranate disease.',
+        'translation_key': 'disease2_translation_key',
+    },
+    3: {
+        'name': 'Cercospora',
+        'description': 'This class represents the second type of pomegranate disease.',
+        'translation_key': 'disease14_translation_key',
+    },
+    4: {
+        'name': 'Healthy',
+        'description': 'This class represents the first type of pomegranate disease.',
+        'translation_key': 'healthy_translation_key',
+    },
+}
 def translate_text_rapidapi(text_to_translate, target_language):
     if target_language == 'en':
         return text_to_translate
@@ -64,6 +103,7 @@ st.set_page_config(
 selected_language_value = st.sidebar.selectbox("Select Language:", list(languages.values()))
 selected_language = next(key for key, value in languages.items() if value == selected_language_value)
 
+
 with st.sidebar:
     st.header("Image Config")     
     source_img = st.file_uploader(
@@ -89,34 +129,49 @@ except Exception as ex:
     st.error(ex)
 
 if st.sidebar.button('Detect Objects'):
-    res = model.predict(uploaded_image,
-                        conf=confidence
-                        )
+    res = model.predict(uploaded_image, conf=confidence)
     boxes = res[0].boxes
+    confidences = boxes.conf
+    class_labels = boxes.cls
+
+    # Display the detected image with bounding boxes
     res_plotted = res[0].plot()[:, :, ::-1]
     with col2:
-        st.image(res_plotted,
-                 caption='Detected Image',
-                 use_column_width=True
-                 )
+        st.image(res_plotted, caption='Detected Image', use_column_width=True)
+
         try:
+            st.markdown("<div style='display: flex; justify-content: center;'>", unsafe_allow_html=True)
             with st.expander("Detection Results"):
-                for box in boxes:
-                    st.write(box.xywh)
+                detected_classes = set()  # Initialize detected_classes set to avoid UndefinedVariable error
+                for i, box in enumerate(boxes):
+                    class_label = int(class_labels[i])  # Convert to int if needed
+                    confidence = float(confidences[i])  # Convert to float if needed
+                    detected_class_info = class_info.get(class_label, {})
+
+                    # Check if the detected class has already been displayed
+                    if detected_class_info['name'] not in detected_classes:
+                        detected_classes.add(detected_class_info['name'])
+
+                        # Display detected class name, description, and confidence
+                        st.markdown(f"<span style='color: blue;'>{detected_class_info.get('name', 'Unknown')}</span>", unsafe_allow_html=True)
+                        # st.write(f"Description: {detected_class_info.get('description', '')}")
+                        # st.write(f"Confidence: {confidence}")
+
+                        # Check if translation is required and show translated description
+                        if selected_language != 'en':
+                            translation_key = detected_class_info.get('translation_key', '')
+                            # translated_class = translate_text_rapidapi(detected_class_info.get('name', ''), selected_language)
+                            translated_description = translate_text_rapidapi(detected_class_info.get('description', ''), selected_language)
+                            # st.write(f"Translated Class: {translated_class}")
+                            st.write(f" {translated_description}")
+                        else:
+                            # translation_key = detected_class_info.get('translation_key', '')  
+                            # st.write(f"Class: {detected_class_info.get('name', '')}")
+                            st.write(f" {detected_class_info.get('description', '')}")
+
+
         except Exception as ex:
-            st.write("No image is uploaded yet!")
-            
-text_to_translate = "It is fruit disease "
-target_language = languages[selected_language]
+            st.write("")
 
-# Use st.spinner to indicate loading
-with st.spinner("Translating..."):
-    if target_language != 'en':  # Only translate if the selected language is not English
-        translated_text = translate_text_rapidapi(text_to_translate, target_language)
-        st.write(f"{translated_text}")
-        
 
-    else:
-        translated_text = text_to_translate
-        st.write(f"{text_to_translate}")
-        
+# /////////////////////////////////////////////////////////////////////////////////////////////////////
